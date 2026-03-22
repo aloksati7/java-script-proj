f5e67d53b99eb2c54ee3a5f91fe4fbe2d6b9a697
aloksati7 <aloksati986@gmail.com>
2026-03-22 12:01:19 +0530

    adding invalid city name feature

--- weather-app/script.js
+++ weather-app/script.js
@@ -10,8 +10,12 @@ const weather_img = document.querySelector(".weather-icon");
 
 async function getWeather(city){
     const resposne = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await resposne.json();
-    console.log(data);
+    if(resposne.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+let data = await resposne.json();
+    
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -36,7 +40,11 @@ async function getWeather(city){
    else if(data.weather[0].main == "Mist"){
     weather_img.src = "images/mist.png"
    }
+   document.querySelector(".weather").style.display = "block";
+   document.querySelector(".error").style.display = "none";
 
+    }
+    
 }
 
 searchbtn.addEventListener("click" ,() => {
